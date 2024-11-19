a1858f4e5baaf46903af2e024aaa7404f4c8648f
Karbbone <clement.maillet56@gmail.com>
2024-11-19 23:25:40 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Select>
+        <SelectTrigger className="w-[180px]">
+          <SelectValue placeholder="Theme" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value="light">Light</SelectItem>
+          <SelectItem value="dark">Dark</SelectItem>
+          <SelectItem value="system">System</SelectItem>
+        </SelectContent>
+      </Select>
+    </>
+  );
+}
+
+export default App;
